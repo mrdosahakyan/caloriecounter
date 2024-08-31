279bd896174ed991b81f13e5f1277313da160ad9
mrdosahakyan <martiros@ultralabs.io>
2024-08-31 13:50:03 +0400

    choose payment method

--- app/ui/components/payment/ChoosePaymentMethod.tsx
+++ app/ui/components/payment/ChoosePaymentMethod.tsx
@@ -1,24 +1,32 @@
-import React, { FC } from 'react';
-import { Select, MenuItem, FormControl, Box, Typography, InputBase } from '@mui/material';
-import { SiApplepay } from 'react-icons/si';
-import { CiCreditCard2 } from 'react-icons/ci';
-import { FaCheck } from 'react-icons/fa';
+import React, { FC } from "react";
+import {
+  Select,
+  MenuItem,
+  FormControl,
+  Box,
+  Typography,
+  InputBase,
+} from "@mui/material";
+import ApplePay from "../../../../public/icons/ApplePay.svg";
+import { FaCheck } from "react-icons/fa";
+import Image from "next/image";
+import { CiCreditCard1 } from "react-icons/ci";
 
 export enum EPaymentMethod {
-  CARD = 'card',
-  APPLE_PAY = 'apple_pay',
+  CARD = "card",
+  APPLE_PAY = "apple_pay",
 }
 
 const options = [
   {
-    label: 'Credit Card',
+    label: "Credit Card",
     value: EPaymentMethod.CARD,
-    icon: <CiCreditCard2 size={42} />, // Increase icon size
+    icon: <CiCreditCard1 fontSize={60} />, // Increase icon size: ;
   },
   {
-    label: 'Apple Pay',
+    label: "Apple Pay",
     value: EPaymentMethod.APPLE_PAY,
-    icon: <SiApplepay size={42} />, // Increase icon size
+    icon: <Image width={60} height={40} src={ApplePay} alt="apple pay" />, // Increase icon size: ;
   },
 ];
 
@@ -43,37 +51,44 @@ const ChoosePaymentMethod: FC<TChoosePaymentMethodProps> = ({
         input={
           <InputBase
             style={{
-              backgroundColor: '#fff9f1',
-              padding: '12px 16px', // Add padding to the select box
-              border: 'none', // Remove border
-              outline: 'none', // Remove outline on focus
-              boxShadow: 'none', // Remove box-shadow
+              backgroundColor: "#FFF5E5",
+              padding: "8px 8px", // Add padding to the select box
+              border: "none", // Remove border
+              outline: "none", // Remove outline on focus
+              boxShadow: "none", // Remove box-shadow
             }}
           />
         }
         MenuProps={{
           anchorOrigin: {
-            vertical: 'top',
-            horizontal: 'left',
+            vertical: "top",
+            horizontal: "left",
           },
           transformOrigin: {
-            vertical: 'bottom',
-            horizontal: 'left',
+            vertical: "bottom",
+            horizontal: "left",
           },
           PaperProps: {
             style: {
               maxHeight: 350, // Increase the dropdown size
-              width: '300px',
-              marginTop: '10px', // Add space between select and dropdown
-              borderRadius: '8px', // Add border radius to dropdown container
+              width: "300px",
+              marginTop: "10px", // Add space between select and dropdown
+              borderRadius: "8px", // Add border radius to dropdown container
             },
           },
         }}
         renderValue={(selected) => (
           <Box display="flex" alignItems="center">
-            {options.find(option => option.value === selected)?.icon}
-            <Typography variant="h5" fontWeight="bold" style={{ marginLeft: '10px' }}>
-              {options.find(option => option.value === selected)?.label}
+            {options.find((option) => option.value === selected)?.icon}
+            <Typography
+              fontWeight="bold"
+              style={{
+                paddingLeft: "18px",
+                fontSize: "20px",
+                color: "#021533",
+              }}
+            >
+              {options.find((option) => option.value === selected)?.label}
             </Typography>
           </Box>
         )}
@@ -83,18 +98,29 @@ const ChoosePaymentMethod: FC<TChoosePaymentMethodProps> = ({
             key={option.value}
             value={option.value}
             style={{
-              backgroundColor: 'transparent', // Remove background color of selected option
-              padding: '18px 16px', // Add padding to the options
+              backgroundColor: "transparent", // Remove background color of selected option
+              padding: "12px 10px", // Add padding to the options
             }}
           >
-            <Box display="flex" alignItems="center" justifyContent="space-between" width="100%">
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+              width="100%"
+            >
               <Box display="flex" alignItems="center">
                 {option.icon}
-                <Typography variant="h5" fontWeight="bold" style={{ marginLeft: '8px' }}>
+                <Typography
+                  variant="h5"
+                  fontWeight="bold"
+                  style={{ marginLeft: "18px" }}
+                >
                   {option.label}
                 </Typography>
               </Box>
-              {option.value === selectedPaymentMethod && <FaCheck size={16} className="text-green-500" />}
+              {option.value === selectedPaymentMethod && (
+                <FaCheck size={16} className="text-green-500" />
+              )}
             </Box>
           </MenuItem>
         ))}
