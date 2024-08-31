0df0a5f2022ef6387202fe7ee1326ec8045e4be5
mrdosahakyan <martiros@ultralabs.io>
2024-08-31 12:22:07 +0400

    a

--- app/page.tsx
+++ app/page.tsx
@@ -76,7 +76,7 @@ export default function Home() {
   const hideHeader = stepsNotInSteper.includes(step);
 
   return (
-    <main className="bg-transparent flex flex-col min-h-screen">
+    <main className="bg-transparent flex flex-col h-screen max-h-screen min-h-screen">
       <Header
         currentStep={step}
         onBack={handleBack}
