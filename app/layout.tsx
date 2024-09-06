92dc4882725750275573b935ebf56992d119d022
mrdosahakyan <martiros@ultralabs.io>
2024-09-06 21:44:11 +0400

    fb pixel initial setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@
 
 import { NextUIProvider } from "@nextui-org/system";
 import "./globals.css";
-import { useEffect } from "react";
+import Script from "next/script";
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -11,6 +12,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+             <!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '878655217470351');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=878655217470351&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
+            `,
+          }}
+        />
+      </Head>
       <body className="bg-primaryBgColor h-svh">
         <NextUIProvider>{children}</NextUIProvider>
       </body>
