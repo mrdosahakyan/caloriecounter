58fda25e4eaa9b58d31e6f0caac7e1e606adc3f5
mrdosahakyan <martiros@ultralabs.io>
2024-09-06 22:20:18 +0400

    fb pixel initial setup 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-"use client";
-
 import { NextUIProvider } from "@nextui-org/system";
 import "./globals.css";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -12,26 +11,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-             <script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '878655217470351');
-fbq('track', 'PageView');
-</script>
-            `,
-          }}
-        />
+        <title>Calorie Counter</title>
       </Head>
       <body className="bg-primaryBgColor h-svh">
+        <NextUIProvider>{children}</NextUIProvider>
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`       
+        !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '878655217470351');
+            fbq('track', 'PageView');             
+            `}
+        </Script>
         <noscript>
           <img
             height="1"
@@ -40,7 +37,6 @@ fbq('track', 'PageView');
             src="https://www.facebook.com/tr?id=878655217470351&ev=PageView&noscript=1"
           />
         </noscript>
-        <NextUIProvider>{children}</NextUIProvider>
       </body>
     </html>
   );
