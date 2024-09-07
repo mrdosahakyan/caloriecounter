f7acc1c6e3cb6d5e7542e19a97d39db6ac428788
mrdosahakyan <martiros@ultralabs.io>
2024-09-07 13:22:31 +0400

    update-payment-success-page

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -3,27 +3,48 @@
 import React from "react";
 import { FaCheckCircle } from "react-icons/fa";
 import { Button } from "@nextui-org/react";
+import Image from "next/image";
+import Warning from "../../public/warning.png";
+import StepperTitle from "../ui/components/stepperLayout/StepperTitle";
 
 const Success = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-[#fff9f1] p-4">
-      <div className="flex flex-col items-center bg-white p-8 rounded-lg shadow-lg">
-        <FaCheckCircle size={64} className="text-green-500 mb-4" />
+    <div className="flex flex-col items-center justify-between h-svh bg-primaryBgColor p-4">
+      <div className="h-full flex flex-col items-center justify-around">
+        <div>
+          <Image
+            src={Warning}
+            alt="warning"
+            width={100}
+            height={100}
+            priority
+          />
+        </div>
 
-        <h1 className="text-2xl text-center font-bold text-gray-800 mb-2">
-          Payment Successful
-        </h1>
-        <p className="text-gray-600 text-center mb-6">
-          Thank you for your purchase! Your transaction has been completed
-          successfully.
-        </p>
+        <div>
+          <StepperTitle>Your payment will be refunded</StepperTitle>
+        </div>
+        <div>
+          <p className="font-inter text-[20px] font-normal text-center text-[#09162E] mb-6 ">
+            We are updating the app and have temporarily suspended new
+            purchases. We apologize for the inconvenience.
+          </p>
+          <p className="font-inter text-[20px] font-normal text-center text-[#09162E] ">
+            Refunds takes 5-10 days to <br />
+            appear on a customer's <br />
+            statement.
+          </p>
+        </div>
+      </div>
+
+      <div className="w-full">
         <Button
           radius="full"
           size="lg"
           onClick={() => {
             window.location.href = "/";
           }}
-          className={`bg-buttonPrimaryBgColor text-white`}
+          className={`bg-buttonPrimaryBgColor text-white w-full`}
         >
           Go to Home Page
         </Button>
