19459f3a572728b1a0031d1e988be4ca77b84e9b
mrdosahakyan <martiros@ultralabs.io>
2024-09-07 12:52:50 +0400

    fb-pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,15 +18,15 @@ export default function RootLayout({
         <Script id="facebook-pixel" strategy="afterInteractive">
           {`       
         !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '878655217470351');
-            fbq('track', 'PageView');             
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '535765588843358');
+fbq('track', 'PageView');            
             `}
         </Script>
         <noscript>
@@ -34,7 +34,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=878655217470351&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=535765588843358&ev=PageView&noscript=1"
           />
         </noscript>
       </body>
